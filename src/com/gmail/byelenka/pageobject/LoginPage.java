5a8f71669dc3027aa51060114e18fb5efbf7761f
Byelenka <byelenka@gmail.com>
2017-09-18 22:19:52 +0300

    'added'

---
+++ src/com/gmail/byelenka/pageobject/LoginPage.java
@@ -0,0 +1,12 @@
+package com.gmail.byelenka.pageobject;
+
+public class LoginPage {
+	static public void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "d:\\Java\\chromedriver.exe");
+	private RemoteWebDriver _driver;
+
+	public LoginPage(RemoteWebDriver driver) {
+
+	}
+	}
+}
